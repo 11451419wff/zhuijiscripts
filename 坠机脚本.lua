567176bbaa451c91cb497d7c1938c85992683319
11451419wff <1994572625@qq.com>
2025-08-05 19:52:31 +0800

    坠机脚本.lua

--- 坠机脚本.lua
+++ 坠机脚本.lua
@@ -25,15 +25,15 @@ local Window = Rayfield:CreateWindow({
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
 
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
       Title = "需要卡密",
       Subtitle = "卡密系统",
-      Note = "输入卡密", -- Use this to tell the user how to get a key
+      Note = "QQ群获取卡密，QQ群群：1055869706，脚本会持续更新", -- Use this to tell the user how to get a key
       FileName = "卡密", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"asfdojhafdo"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = {"Kab"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
